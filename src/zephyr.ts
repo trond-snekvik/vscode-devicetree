db489acd5a702b17376b001452123488ba3231a5
Trond Einar Snekvik <Trond.Einar.Snekvik@nordicsemi.no>
2021-06-25 16:41:29 +0200

    Rework zephyr resolution procedure
    Removes the need for west, and uses modules for finding boards and
    bindings

--- src/zephyr.ts
+++ src/zephyr.ts
@@ -5,171 +5,275 @@
  */
 import * as vscode from 'vscode';
 import * as path from 'path';
+import * as os from 'os';
 import { env } from 'process';
-import { ExecOptions, exec } from 'child_process';
-import { existsSync, readFileSync } from 'fs';
+import { exec } from 'child_process';
+import { existsSync, readdir, readFile, readFileSync } from 'fs';
 import * as glob from 'glob';
 import * as yaml from 'js-yaml';
+import { config } from './config';
+import { Profiler } from './util';
 
-export type BoardInfo = { identifier: string, name: string, type: string, arch: string, toolchain: string[], ram: number, flash: number, supported: string[] };
-export type Board = { name: string, path: string, arch?: string, info?: BoardInfo | {[name: string]: any} };
-const conf = vscode.workspace.getConfiguration();
+export type BoardInfo = {
+    identifier: string;
+    name: string;
+    type: string;
+    arch: string;
+    toolchain: string[];
+    ram: number;
+    flash: number;
+    supported: string[];
+};
+export type Board = { name: string; uri: vscode.Uri; arch?: string; info?: BoardInfo | { [name: string]: any } };
 export let zephyrRoot: string;
-let westExe: string;
-let westVersion: string;
 let boards: Board[];
-export let modules: string[];
+export let modules: vscode.Uri[];
 
-function west(...args: string[]): Promise<string> {
+async function getCMakePackages(): Promise<vscode.Uri[]> {
+    const packageToZephyrURI = (entry: string) => {
+        return vscode.Uri.file(path.resolve(entry, '..', '..', '..'));
+    };
 
-	const command = westExe + ' ' + args.join(' ');
-
-	const options: ExecOptions = {
-		cwd: zephyrRoot ?? vscode.workspace.workspaceFolders?.find(w => w.name.match(/zephyr/i))?.uri.fsPath ?? vscode.workspace.workspaceFolders?.[0]?.uri.fsPath,
-	};
-
-	return new Promise<string>((resolve, reject) => {
-		exec(command, options, (err, out) => {
-			if (err) {
-				reject(err);
-			} else {
-				resolve(out);
-			}
-		});
-	});
+    if (process.platform === 'win32') {
+        return new Promise(resolve => {
+            exec('reg query HKCU\\Software\\Kitware\\CMake\\Packages\\Zephyr', (err, out) => {
+                if (err) {
+                    resolve([]);
+                } else {
+                    resolve(
+                        out
+                            .split('\n')
+                            .filter(line => line.includes('REG_SZ')) // <hash> REG_SZ <zephyr-path/share/zephyr-package/cmake>
+                            .map(line => packageToZephyrURI(line.trim().split(' ').pop()))
+                    );
+                }
+            });
+        });
+    } else {
+        const packages = path.resolve(os.homedir(), '.cmake', 'packages', 'Zephyr');
+        const entries = await new Promise<string[]>(resolve => readdir(packages, (err, out) => resolve(err ? [] : out)));
+        const uris = new Array<vscode.Uri>();
+        await Promise.all(
+            entries.map(
+                entry =>
+                    new Promise<void>(resolve => {
+                        readFile(path.resolve(packages, entry), 'utf-8', (err, out) => {
+                            if (!err) {
+                                uris.push(packageToZephyrURI(out.trim()));
+                            }
+                            resolve();
+                        });
+                    })
+            )
+        );
+        return uris;
+    }
 }
 
-export function openConfig(entry: string) {
-	vscode.commands.executeCommand('workbench.action.openSettings', entry);
+async function findZephyr() {
+    if (config.get('zephyr')) {
+        await setZephyrBase(vscode.Uri.file(config.get('zephyr')));
+        return;
+    }
+
+    if (env['ZEPHYR_BASE']) {
+        await setZephyrBase(vscode.Uri.file(env['ZEPHYR_BASE'] as string));
+        return;
+    }
+
+    const cmakePackages = await getCMakePackages();
+    if (cmakePackages.length === 1) {
+        await setZephyrBase(cmakePackages[0]);
+        return;
+    }
+
+    const pack = cmakePackages.find(uri => {
+        // The zephyr folder is inside this workspace:
+        if (vscode.workspace.getWorkspaceFolder(uri)) {
+            return true;
+        }
+
+        // or some folder in this workspace is in this zephyr instance:
+        const topdir = path.dirname(uri.fsPath);
+        return vscode.workspace.workspaceFolders?.some(folder => !path.relative(topdir, folder.uri.fsPath).startsWith('..'));
+    });
+    if (pack) {
+        await setZephyrBase(pack);
+        return;
+    }
+
+    if (cmakePackages.length > 1) {
+        await setZephyrBase(cmakePackages[0]);
+
+        const buttons = { setDefault: 'Use as default', change: 'Change' };
+        vscode.window
+            .showInformationMessage(`Using Zephyr installation at ${zephyrRoot}`, ...Object.values(buttons))
+            .then(async button => {
+                switch (button) {
+                    case buttons.change:
+                        vscode.window.showQuickPick([...cmakePackages.map(uri => uri.fsPath), 'Browse...']).then(pick => {
+                            if (pick === 'Browse...') {
+                                vscode.window
+                                    .showOpenDialog({
+                                        openLabel: 'Set',
+                                        canSelectFiles: false,
+                                        canSelectFolders: true,
+                                    })
+                                    .then(
+                                        folder => {
+                                            config.set('zephyr', zephyrRoot);
+                                            setZephyrBase(folder[0]);
+                                        },
+                                        () => undefined
+                                    );
+                            } else {
+                                setZephyrBase(vscode.Uri.file(pick));
+                            }
+                        });
+                        break;
+                    case buttons.setDefault:
+                        config.set('zephyr', zephyrRoot);
+                        break;
+                }
+            });
+        return;
+    }
+
+    vscode.window.showErrorMessage(`Couldn't find Zephyr root`, 'Configure...').then(button => {
+        if (button) {
+            config.configureSetting('zephyr');
+        }
+    });
 }
 
-async function findWest() {
-	if (!(westExe = conf.get('devicetree.west') as string) &&
-		!(westExe = conf.get('kconfig.zephyr.west') as string)) {
-		westExe = 'west';
-	}
+function resolveModules(): vscode.Uri[] {
+    const dirs = config.get('modules');
+    const uris = new Array<vscode.Uri>();
+    dirs.map(d =>
+        d
+            .replace(/\${(.*?)}/g, (original, name: string) => {
+                if (name === 'workspaceFolder') {
+                    return vscode.workspace.workspaceFolders?.[0].uri.fsPath ?? vscode.env.appRoot;
+                }
 
-	return west('-V').then(version => {
-		westVersion = version.match(/v\d+\.\d+\.\d+/)?.[0];
-	}, (err: Error) => {
-		vscode.window.showErrorMessage(`Couldn't find west (${err.name})`, 'Configure west path...').then(() => {
-			openConfig('devicetree.west');
-		});
-	});
+                if (name.startsWith('workspaceFolder:')) {
+                    const folder = name.split(':')[1];
+                    return vscode.workspace.workspaceFolders.find(w => w.name === folder)?.uri.fsPath ?? original;
+                }
+
+                if (name.toLowerCase().startsWith('env:')) {
+                    const variable = name.split(':')[1];
+                    return process.env[variable] ?? '';
+                }
+
+                if (['zephyr_base', 'zephyrbase', 'zephyr'].includes(name.toLowerCase())) {
+                    return zephyrRoot;
+                }
+
+                return original;
+            })
+            .replace(/^~/, os.homedir())
+    ).forEach(p => {
+        if (path.isAbsolute(p)) {
+            uris.push(vscode.Uri.file(p));
+        } else {
+            vscode.workspace.workspaceFolders?.forEach(workspace => uris.push(vscode.Uri.joinPath(workspace.uri, p)));
+        }
+    });
+
+    return uris;
 }
 
-async function findZephyrRoot() {
-	if (!(zephyrRoot = conf.get('devicetree.zephyr') as string) &&
-		!(zephyrRoot = conf.get('kconfig.zephyr.base') as string) &&
-		!(zephyrRoot = env['ZEPHYR_BASE'] as string)) {
-		return Promise.all([west('topdir'), west('config', 'zephyr.base')]).then(([topdir, zephyr]) => {
-			zephyrRoot = path.join(topdir.trim(), zephyr.trim());
-		}, err => {
-			vscode.window.showErrorMessage(`Couldn't find Zephyr root`, 'Configure...').then(() => {
-				openConfig('devicetree.zephyr');
-			});
-		});
-	}
+export async function setZephyrBase(uri: vscode.Uri) {
+    zephyrRoot = uri.fsPath;
+    modules = resolveModules();
+    boards = resolveBoards();
+    zephyrEmitter.fire(zephyrRoot);
 }
 
 export function findBoard(board: string): Board | undefined {
-	return boards.find(b => b.name === board);
+    return boards.find(b => b.name === board);
 }
 
 export async function isBoardFile(uri: vscode.Uri) {
-	if (path.extname(uri.fsPath) !== '.dts') {
-		return false;
-	}
+    if (path.extname(uri.fsPath) !== '.dts') {
+        return false;
+    }
 
-	for (const root of boardRoots()) {
-		if (uri.fsPath.startsWith(path.normalize(root))) {
-			return true;
-		}
-	}
-
-	return false;
+    return boardRoots().some(root => !path.relative(root.fsPath, uri.fsPath).startsWith('..'));
 }
 
 export async function defaultBoard(): Promise<Board> {
-	const dtsBoard = conf.get('devicetree.defaultBoard') as string;
-	if (dtsBoard) {
-		const path = findBoard(dtsBoard);
-		if (path) {
-			console.log('Using default board');
-			return path;
-		}
-	}
+    const dtsBoard = config.get('defaultBoard') as string;
+    if (dtsBoard) {
+        const path = findBoard(dtsBoard);
+        if (path) {
+            console.log('Using default board');
+            return path;
+        }
+    }
 
-	const kconfigBoard = conf.get('kconfig.zephyr.board') as { board: string, arch: string, dir: string };
-	if (kconfigBoard?.dir && kconfigBoard.board) {
-		const board = <Board>{ name: kconfigBoard.board, path: path.join(kconfigBoard.dir, kconfigBoard.board + '.dts'), arch: kconfigBoard.arch };
-		if (existsSync(board.path)) {
-			console.log('Using Kconfig board');
-			return board;
-		}
-	}
-
-	console.log('Using fallback board');
-	return findBoard('nrf52dk_nrf52832') ?? findBoard('nrf52_pca10040');
+    return findBoard('nrf52dk_nrf52832') ?? findBoard('nrf52_pca10040');
 }
 
-function boardRoots(): string[] {
-	return modules.map(m => m + '/boards').filter(dir => existsSync(dir));
+function boardRoots(): vscode.Uri[] {
+    return modules.map(m => vscode.Uri.joinPath(m, 'boards')).filter(dir => existsSync(dir.fsPath));
 }
 
-async function findBoards() {
-	boards = new Array<Board>();
-	return Promise.all(boardRoots().map(root => new Promise<void>(resolve => glob(`**/*.dts`, { cwd: root }, (err, matches) => {
-		if (!err) {
-			matches.forEach(m => boards.push({name: path.basename(m, '.dts'), path: `${root}/${m}`, arch: m.split(/[/\\]/)?.[0]}));
-		}
+export function board(uri: vscode.Uri): Board {
+    const name = path.basename(uri.fsPath, path.extname(uri.fsPath));
+    const arch = path.basename(path.resolve(path.dirname(uri.fsPath), '..'));
 
-		resolve();
-	}))));
+    return {
+        name,
+        uri,
+        arch,
+    };
 }
 
-async function loadModules() {
-	modules = await west('list', '-f', '{posixpath}').then(out => out.split(/\r?\n/).map(line => line.trim()), _ => []);
-	await findBoards();
+function resolveBoards() {
+    const boards = new Array<Board>();
+    const roots = boardRoots();
+    roots.forEach(root => {
+        const matches = glob.sync('**/*_defconfig', { cwd: root.fsPath, absolute: true });
+        matches.forEach(m => {
+            const dir = vscode.Uri.file(path.dirname(m));
+            const dts = vscode.Uri.joinPath(dir, path.basename(m, '_defconfig') + '.dts');
+            boards.push(board(dts));
+        });
+    });
+
+    return boards;
 }
 
-export async function selectBoard(prompt='Set board'): Promise<Board> {
-	return vscode.window.showQuickPick(boards.map(board => <vscode.QuickPickItem>{ label: board.name, description: board.arch, board }), { placeHolder: prompt }).then(board => board['board']);
+export async function selectBoard(prompt = 'Set board'): Promise<Board> {
+    return vscode.window
+        .showQuickPick(
+            boards.map(board => <vscode.QuickPickItem>{ label: board.name, description: board.arch, board }),
+            { placeHolder: prompt }
+        )
+        .then(board => board['board']);
 }
 
-export async function activate(ctx: vscode.ExtensionContext) {
-	await findWest();
-	await findZephyrRoot();
-	if (zephyrRoot) {
-		await loadModules();
-		return;
-	}
-
-	return new Promise<void>(resolve => {
-		ctx.subscriptions.push(vscode.workspace.onDidChangeConfiguration(async e => {
-			if (e.affectsConfiguration('kconfig.zephyr.base') || e.affectsConfiguration('kconfig.zephyr.west') ||
-				e.affectsConfiguration('devicetree.zephyr') || e.affectsConfiguration('devicetree.west')) {
-				await findWest();
-				await findZephyrRoot();
-				if (zephyrRoot) {
-					await loadModules();
-					resolve();
-				}
-			}
-		}));
-	});
+export function activate(ctx: vscode.ExtensionContext) {
+    config.onChange('modules', () => (modules = resolveModules()));
+    config.onChange('zephyr', findZephyr);
+    return findZephyr();
 }
+
+const zephyrEmitter = new vscode.EventEmitter<string>();
+export const onChange = zephyrEmitter.event;
 
 export function resolveBoardInfo(board: Board) {
-	const file = path.join(path.dirname(board.path), board.name + '.yaml');
-	if (!existsSync(file)) {
-		return;
-	}
+    const file = vscode.Uri.joinPath(board.uri, '..', board.name + '.yaml');
+    if (!existsSync(file.fsPath)) {
+        return;
+    }
 
-	const out = readFileSync(file, 'utf-8');
-	if (!out) {
-		return;
-	}
+    const out = readFileSync(file.fsPath, 'utf-8');
+    if (!out) {
+        return;
+    }
 
-	board.info = <BoardInfo>yaml.load(out, { json: true }) || {};
+    board.info = <BoardInfo>yaml.load(out, { json: true }) || {};
 }
